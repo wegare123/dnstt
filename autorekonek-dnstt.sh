7f5e64d64b8b3013d3494d926d884e59f45b6d33
Wega Regianto <56117745+wegare123@users.noreply.github.com>
2022-10-23 08:32:14 +0700

    Add files via upload

--- autorekonek-dnstt.sh
+++ autorekonek-dnstt.sh
@@ -2,7 +2,7 @@
 #dnstt (Wegare)
 route="$(route | grep -i 1.1.1.1 | head -n1 | awk '{print $2}')" 
 route2="$(route | grep -i 10.0.0.2 | head -n1 | awk '{print $2}')" 
-route3="$(netstat -plantu | grep -i ssh | grep -i 10808 | grep -i listen)" 
+route3="$(netstat -plantu | grep -i xray | grep -i 10808 | grep -i listen)" 
 route4="$(netstat -plantu | grep dns-client)" 
 
 echo $route
