f66a189114b109cd74a47cc4eacb8eef0360809a
Wega Regianto <56117745+wegare123@users.noreply.github.com>
2022-10-23 08:24:59 +0700

    Add files via upload

---
+++ dnstt.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+#dnstt (Wegare)
+stop () {
+host=$(cat /root/akun/jsondnstt.json | grep "Host" | tr '"' ' ' | awk '{print $3}')
+route="$(cat /root/akun/ipmodem.txt | grep -i ipmodem | cut -d= -f2 | tail -n1)" 
+killall -q badvpn-tun2socks dns-client fping
+route del 1.1.1.1 gw "$route" metric 0 2>/dev/null
+route del "$host" gw "$route" metric 0 2>/dev/null
+ip link delete tun1 2>/dev/null
+/etc/init.d/dnsmasq restart 2>/dev/null
+}
+mkdir -p /root/akun/jsondnstt.json
+ns2="$(cat /root/akun/dnstt.txt | tr '\n' ' '  | awk '{print $1}')" 
+udp2="$(cat /root/akun/dnstt.txt | tr '\n' ' '  | awk '{print $2}')"
+pubkey2="$(cat /root/akun/server.pub)" 
+clear
+echo "Inject dnstt by wegare"
+echo "1. Sett Profile"
+echo "2. Start Inject"
+echo "3. Stop Inject"
+echo "4. Enable auto booting & auto rekonek"
+echo "5. Disable auto booting & auto rekonek"
+echo "e. exit"
+read -p "(default tools: 2) : " tools
+[ -z "${tools}" ] && tools="2"
+if [ "$tools" = "1" ]; then
+
+echo "Masukkan NS" 
+read -p "default NS: $ns2 : " ns
+[ -z "${ns}" ] && ns="$ns2"
+
+echo "Masukkan pubkey" 
+read -p "default pubkey: $pubkey2 : " pubkey
+[ -z "${pubkey}" ] && pubkey="$pubkey2"
+
+read -p "ingin menggunakan port udpgw y/n " pilih
+if [ "$pilih" = "y" ]; then
+echo "Masukkan port udpgw" 
+read -p "default udpgw: $udp2 : " udp
+[ -z "${udp}" ] && udp="$udp2"
+badvpn="badvpn-tun2socks --tundev tun1 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr 127.0.0.1:10808 --udpgw-remote-server-addr 127.0.0.1:$udp --udpgw-connection-buffer-size 65535 --udpgw-transparent-dns &"
+elif [ "$pilih" = "Y" ]; then
+echo "Masukkan port udpgw" 
+read -p "default udpgw: $udp2 : " udp
+[ -z "${udp}" ] && udp="$udp2"
+badvpn="badvpn-tun2socks --tundev tun1 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr 127.0.0.1:10808 --udpgw-remote-server-addr 127.0.0.1:$udp --udpgw-connection-buffer-size 65535 --udpgw-transparent-dns &"
+else
+badvpn='tun2socks -tunAddr "10.0.0.1" -tunGw  "10.0.0.2" -tunMask "255.255.255.0" -tunName "tun1" -tunDns "8.8.8.8,8.8.4.4" -proxyType "socks" -proxyServer "127.0.0.1:10808" &'
+fi
+
+echo ""
+echo "Masukkan config json ke file /root/akun/jsondnstt.json"
+echo ""
+
+echo "$pubkey" > /root/akun/server.pub
+echo "$ns
+$udp" > /root/akun/dnstt.txt
+cat <<EOF> /usr/bin/gproxy-dnstt
+$badvpn
+EOF
+chmod +x /usr/bin/gproxy-dnstt
+echo "Sett Profile Sukses"
+sleep 2
+clear
+/usr/bin/dnstt
+elif [ "${tools}" = "2" ]; then
+cek="$(cat /root/akun/dnstt.txt)"
+if [[ -z $cek ]]; then
+echo "anda belum membuat profile"
+exit
+fi
+stop
+ipmodem="$(route -n | grep -i 0.0.0.0 | head -n1 | awk '{print $2}')" 
+echo "ipmodem=$ipmodem" > /root/akun/ipmodem.txt
+host=$(cat /root/akun/jsondnstt.json | grep "Host" | tr '"' ' ' | awk '{print $3}')
+ns="$(cat /root/akun/dnstt.txt | tr '\n' ' '  | awk '{print $1}')" 
+route="$(cat /root/akun/ipmodem.txt | grep -i ipmodem | cut -d= -f2 | tail -n1)"
+nohup dns-client -udp 1.1.1.1:53 -pubkey-file /root/akun/server.pub $ns 127.0.0.1:2222 > /dev/null 2>&1 &
+sleep 3
+xray run -c /root/akun/jsondnstt.json &
+sleep 5
+echo ""
+echo "is connecting to the internet"
+ip tuntap add dev tun1 mode tun
+ifconfig tun1 10.0.0.1 netmask 255.255.255.0
+/usr/bin/gproxy-dnstt
+route add 1.1.1.1 gw "$route" metric 0
+route add "$host" gw "$route" metric 0
+route add default gw 10.0.0.2 metric 0
+    
+echo "#!/bin/bash
+#stl (Wegare)
+host=$(cat /root/akun/dnstt.txt | tr '\n' ' '  | awk '{print $1}')
+fping -l $host" > /usr/bin/ping-dnstt
+chmod +x /usr/bin/ping-dnstt
+/usr/bin/ping-dnstt > /dev/null 2>&1 &
+elif [ "${tools}" = "3" ]; then
+stop
+echo "Stop Suksess"
+sleep 2
+clear
+/usr/bin/dnstt
+elif [ "${tools}" = "4" ]; then
+cat <<EOF>> /etc/crontabs/root
+
+# BEGIN AUTOREKONEKDNSTT
+*/1 * * * *  autorekonek-dnstt
+# END AUTOREKONEKDNSTT
+EOF
+sed -i '/^$/d' /etc/crontabs/root 2>/dev/null
+/etc/init.d/cron restart
+echo "Enable Suksess"
+sleep 2
+clear
+/usr/bin/dnstt
+elif [ "${tools}" = "5" ]; then
+sed -i "/^# BEGIN AUTOREKONEKDNSTT/,/^# END AUTOREKONEKDNSTT/d" /etc/crontabs/root > /dev/null
+/etc/init.d/cron restart
+echo "Disable Suksess"
+sleep 2
+clear
+/usr/bin/dnstt
+elif [ "${tools}" = "e" ]; then
+clear
+exit
+else 
+echo -e "$tools: invalid selection."
+exit
+fi
---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#dnstt (Wegare)
+printf 'ctrl+c' | crontab -e > /dev/null
+opkg update && opkg install unzip
+cek=$(cat /etc/openwrt_r*)
+if [[ $cek == *"LEDE"* ]] && [[ $cek == *"ar71xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"brcm63xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/brcm63xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/brcm63xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/brcm63xx
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"ramips"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"sunxi"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"ar71xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"brcm63xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/brcm63xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/brcm63xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/brcm63xx
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"ramips"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"sunxi"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"aarch64_cortex-a53"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mips_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mipsel_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+####elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"x86"* ]]; then
+#wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/i386-pentium.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/i386-pentium/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/i386-pentium
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"x86_64"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/x86_64.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/x86_64/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/x86_64
+else
+echo -e "version anda tidak terdeteksi!"
+exit
+fi
+cek2=$(opkg list-installed | grep dnsmasq-full | awk '{print $1}')
+if [ $cek2 = "dnsmasq-full" ]; then
+echo > /dev/null
+else
+opkg remove dnsmasq && opkg install dnsmasq-full
+fi
+mkdir -p /usr/share/xray/
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/dnstt/main/dnstt.sh" -O /usr/bin/dnstt
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/dnstt/main/autorekonek-dnstt.sh" -O /usr/bin/autorekonek-dnstt
+wget --no-check-certificate "https://github.com/wegare123/dnstt/blob/main/dns-client-v19?raw=true" -O /usr/bin/dns-client
+wget --no-check-certificate "https://github.com/wegare123/vless/blob/main/xray?raw=true" -O /usr/bin/xray
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/geoip.dat" -O /usr/share/xray/geoip.dat
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/geosite.dat" -O /usr/share/xray/geosite.dat
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/tun2socks?raw=true" -O /usr/bin/tun2socks
+chmod +x /usr/bin/dnstt
+chmod +x /usr/bin/dns-client
+chmod +x /usr/bin/autorekonek-dnstt
+opkg install opkg install ip-full && opkg install --force-depends *.ipk && opkg install lsof && opkg install fping screen python python3 coreutils-nohup
+rm -r ~/*.ipk
+rm -r ~/install.sh
+mkdir -p ~/akun/
+touch ~/akun/dnstt.txt
+touch ~/akun/ipmodem.txt 
+sleep 2
+echo "install selesai"
+echo "untuk memulai tools silahkan jalankan perintah 'dnstt'"
+				
